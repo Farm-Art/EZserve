0d1532c74775e393d30afa0323ade4036b0f6361
Farm-Art <github@farmart.tech>
2024-10-02 23:53:08 +0300

    add password protection option;

--- README.md
+++ README.md
@@ -11,11 +11,11 @@ Running the package will serve the current working directory publicly on `0.0.0.
 
 Although I've attempted to prevent abuse by completely banning `..` from the paths, I can't guarantee that there's no way to access files you  
 
-## Roadmap
+## Roadmap (in no particular order)
 
 - [x] **Core:** Proof-of-concept: serve listed files and dirs
 - [ ] **Core:** Figure out HTTPS
-- [ ] **Core:** Add global password protection
+- [x] **Core:** Add global password protection
 - [ ] **Core:** Add per-file password protection
 - [ ] **QoL:** improve the web UI
 - [ ] **QoL:** allow selecting multiple files and folders to download all of them in one ZIP file
@@ -24,10 +24,15 @@ Although I've attempted to prevent abuse by completely banning `..` from the pat
 
 ## Known Issues
 
-I use symlinks to simplify sharing multiple distinct folders as if they were next to each other. Windows, for some reason, is being really stingy about that. Enabling developer mode in settings seems to alleviate the issue; I'm not actively planning on fixing that, although it would be a nice thing to have.
+* I use symlinks to simplify sharing multiple distinct folders as if they were next to each other. Windows, for some reason, is being really stingy about that. Enabling developer mode in settings seems to alleviate the issue; I'm not actively planning on fixing that, although it would be a nice thing to have.
+
+* Large file downloads can sometimes hang the server, rendering it unresponsive to other clients. I'm looking into that one, but it seems mostly harmless in the general use case.
+
+* I'm aware that inserting the password into the URL is insecure, but so is plain HTTP. Get over it.
 
 ## CLI Options
 
 * `-H` `--host` - hostname to listen on. `0.0.0.0` by default, meaning your files are exposed to the internet. It can be a fully qualified domain name (FQDN) or the public IP address of the interface you intend to listen on.
-* `-p` `--port` - port number to listen on. `53443` by default. Any unoccupied port works; your browser will automatically try port `80` for `http://` and port `433` for `https://`.
+* `-p` `--port` - port number to listen on. `53443` by default. Any unoccupied port works; your browser will automatically try port `80` for `http://` and port `443` for `https://`.
 * `-e` `--expand-root` - when serving a single directory, the web UI will show its contents on the frontpage, instead of a list with only the root folder's name. Ignored if you serve a single file/anything more than just a single directory. At the time of writing this, it also prevents the creation of a temp folder with a symlink; this is an implementation detail and should not be relied on (although I doubt anyone will rely on it anyway).
+* `-P` `--password` - sets a global password or generates a random one if `*` is passed as the value. Users must pass `password` as a query parameter to every request, otherwise they will face a 404 error. It's inserted into every hyperlink.
--- src/ezserve/__main__.py
+++ src/ezserve/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import pathlib as p
+import secrets
 import tempfile
 
 import bottle as b
@@ -9,25 +10,22 @@ def abs_path(path: str) -> p.Path:
     return p.Path(path).absolute()
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-H', '--host', default='0.0.0.0', help='host to listen on')
-parser.add_argument('-p', '--port', default=53443, type=int, help='port to listen on')
-parser.add_argument(
-    'files', nargs='*',
-    default=[p.Path.cwd()], type=abs_path,
-    help='files and dirs to serve'
-)
-parser.add_argument(
-    '-e', '--expand-root',
-    action='store_true',
-    help='when serving a single dir, immediately show contents instead; ignored otherwise'
-)
+def password_input(string: str | None) -> str | None:
+    if string is None:
+        return None
 
-args = parser.parse_args()
+    if string == '*':
+        return secrets.token_hex(32)
+
+    return string
 
 
 @b.get('<path:path>')
-def serve(path):
+def serve_files(path):
+    if args.password:
+        if b.request.query.password != args.password:
+            b.abort(404)
+
     path = p.Path(path[1:])
 
     # Preliminary abuse protection (are there even other hazards?)
@@ -40,28 +38,71 @@ def serve(path):
         b.abort(404)
 
     if file.is_dir():
-        return render_dir(file)
+        return render_dir(file, args.password)
     return b.static_file(file.name, file.parent)
 
 
-def render_dir(filepath):
+def list_item(f: p.Path, password: str | None) -> str:
+    return f'<a href="/{f.relative_to(root)}{get_password_string(password)}">{f.name}</a>'
+
+
+def back_button(f: p.Path, password: str | None) -> str:
+    return f'<a href="/{f.relative_to(root).parent}{get_password_string(password)}">Back</a>'
+
+
+def render_dir(filepath: p.Path, password: str | None):
     out = ''
     if filepath != root:
-        out += f'<a href="/{filepath.relative_to(root).parent}">Back</a><br>'
+        out += f'{back_button(filepath, password)}<br>'
 
-    return out + '<br>'.join(f'<a href="/{f.relative_to(root)}">{f.name}</a>' for f in filepath.iterdir())
+    return out + '<br>'.join(
+        list_item(f, password) for f in filepath.iterdir())
 
 
-if all((len(args.files) == 1, args.files[0].is_dir(), args.expand_root)):
-    root = args.files[0]
-    b.run(host=args.host, port=args.port)
-else:
-    with tempfile.TemporaryDirectory() as tmpdir:
-        root = p.Path(tmpdir).absolute()
-        for content_root in args.files:
-            (root / content_root.name).symlink_to(
-                content_root,
-                target_is_directory=content_root.is_dir()
-            )
+def serve(host='0.0.0.0', port=53443, password: str | None = None, quiet: bool = False):
+    if not quiet:
+        print(f'Serving on http://{host}:{port}/.\nCtrl-C to quit.')
+        if password:
+            print(f'Append `{get_password_string(password)}` at the end of your URL to access files.')
+    b.run(host=args.host, port=args.port, quiet=True)
 
-        b.run(host=args.host, port=args.port)
+
+def get_password_string(password: str | None) -> str:
+    return f'?password={password}' if password else ''
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-H', '--host', default='0.0.0.0', help='host to listen on')
+    parser.add_argument('-p', '--port', default=53443, type=int, help='port to listen on')
+    parser.add_argument(
+        'files', nargs='*',
+        default=[p.Path.cwd()], type=abs_path,
+        help='files and dirs to serve'
+    )
+    parser.add_argument(
+        '-e', '--expand-root',
+        action='store_true',
+        help='when serving a single dir, immediately show contents instead; ignored otherwise'
+    )
+    parser.add_argument(
+        '-P', '--password',
+        type=password_input,
+        help='password to protect files; set to * to generate a random one'
+    )
+
+    args = parser.parse_args()
+
+
+    if all((len(args.files) == 1, args.files[0].is_dir(), args.expand_root)):
+        root = args.files[0]
+        serve(args.host, args.port, args.password)
+    else:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            root = p.Path(tmpdir).absolute()
+            for content_root in args.files:
+                (root / content_root.name).symlink_to(
+                    content_root,
+                    target_is_directory=content_root.is_dir()
+                )
+            serve(args.host, args.port, args.password)
